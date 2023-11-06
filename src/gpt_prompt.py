3744c51612d73176b8508b8da721657bff209791
Yinyihong <yl880@duke.edu>
2023-11-06 13:10:24 -0500

    update playground

--- src/gpt_prompt.py
+++ src/gpt_prompt.py
@@ -58,6 +58,57 @@ def get_sentiment_general(news, model="gpt-3.5-turbo", company=None, output="pol
     return output, reason
 
 
+def get_sentiment_general_multiple(news_list, model="gpt-3.5-turbo", company=None, output="polar", explanation=False, threads=10):
+    _ = load_dotenv(find_dotenv())
+    openai.api_key  = os.getenv('API_KEY')   
+    if company:
+        company_prompt = f"for {company}" 
+    else:
+        company_prompt = ""
+    if explanation:
+        explanation_prompt = f"Your answer will include 2 lines. In the first line, you will answer 1 sentence to analyze why the news is good or bad {company_prompt}."
+        explanation_addup = "in the second line, "
+    else:
+        explanation_prompt = "You will not give explanation to your answer."
+        explanation_addup = ""
+        
+    if output == "polar":
+        output_prompt = f"Then, {explanation_addup}you will answer with: BEARISH, BULLISH, NEUTRAL."
+    elif output =="score":
+        output_prompt = f"Then, {explanation_addup}you will answer with an integer between 1 and 10, with 1 being most BEARISH, 10 being most BULLISH."
+    else:
+        output_prompt = f"Then, {explanation_addup}you will answer with an integer between 1 and 10, with 1 being most BEARISH, 10 being most BULLISH."
+        
+    system_analysis_prompt =  f"You will work as a Sentiment Analysis for Financial News {company_prompt}. {explanation_prompt} {output_prompt}"
+    messages = []
+    messages = [{"role": "system", "content": system_analysis_prompt}]
+    for news in news_list:
+        messages.append({"role": "user", "content":news})
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    print(response)
+    response_message = response.choices[0].message["content"]
+    if explanation:
+        ans = response_message.split("\n")
+        if len(ans) == 1:
+            ans = response_message.split(" ")
+            output = ans[-1]
+            reason = " ".join(ans[:-1])
+        else:
+            reason = ans[0]
+            output = ans[-1]
+        
+    else:
+        output = response_message
+        reason = ""
+    
+    return output, reason
+
+
+
 def get_sentiment_general_parallel(news_list, model="gpt-3.5-turbo", company=None, output="polar", explanation=False, threads=10):
     t0 = time.time()
     with Pool(threads) as p:
