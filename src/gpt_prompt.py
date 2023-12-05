629f7cd4435d72c091f5d8afc9aab2746f8f11e8
Yiyang <yiyangshao99@gmail.com>
2023-12-05 20:21:50 +0800

    finish risk management attempt1

--- src/gpt_prompt.py
+++ src/gpt_prompt.py
@@ -138,14 +138,14 @@ def get_risk_score(news, model="gpt-3.5-turbo", company=None, output="score", ex
     else:
         company_prompt = ""
     if explanation:
-        explanation_prompt = f"Your answer will include 2 lines. In the first line, you will answer 1 sentence to analyze why the news might be risky {company_prompt}."
+        explanation_prompt = f"Your answer will include 2 lines. In the first line, you will answer 1 sentence to give solid reason why the news is highly risky {company_prompt}."
         explanation_addup = "in the second line, "
     else:
         explanation_prompt = "You will not give explanation to your answer."
         explanation_addup = ""
         
     if output == "polar":
-        output_prompt = f"Then, {explanation_addup}you will answer with: RISKY, NEUTRAL, SAFE."
+        output_prompt = f"Then, {explanation_addup}you will answer with: **HIGH RISK** or **LOW RISK**"
     elif output =="score":
         output_prompt = f"Then, {explanation_addup}you will answer with an integer between 1 and 100, with 1 being most safe, 100 being most risky."
    
@@ -174,6 +174,16 @@ def get_risk_score(news, model="gpt-3.5-turbo", company=None, output="score", ex
         reason = ""
     
     return output, reason
+
+def get_risk_score_parallel(news_list, model="gpt-3.5-turbo", company=None, output="score", explanation=False, threads=10):
+    t0 = time.time()
+    with Pool(threads) as p:
+        pool_output = p.starmap(get_risk_score, zip(news_list, repeat(model), repeat(company), repeat(output), repeat(explanation)))
+    t1 = time.time()
+    print(f" average running time: {(t1-t0)/len(news_list):.2f} second")
+    return pool_output
+
+
 
 def get_sentiment_conservative(news, model="gpt-3.5-turbo", company=None, output="polar", explanation=False):
     _ = load_dotenv(find_dotenv())
